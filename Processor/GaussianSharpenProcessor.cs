48748f73ef3bed407cdb2bf9f01335bc28c61b8e
mx1700 <mx1700@gmail.com>
2025-02-28 21:26:58 +0800

    参数 v 参数，用已刷新缓存

--- Processor/GaussianSharpenProcessor.cs
+++ Processor/GaussianSharpenProcessor.cs
@@ -8,8 +8,10 @@ public class GaussianSharpenProcessor: IImageWebProcessor
 {
     public const string Sharpen = "sharpen";
     
+    //TODO: v 表示版本，其实并不是用来处理 Processor，因为缓存 key 是通过 Commands 列表计算的，当图片被替换时，key 不变，导致一直读取缓存
+    //这时可以通过增加 v=新值 来读取新图片
     private static readonly IEnumerable<string> SharpenCommands
-        = [Sharpen];
+        = [Sharpen, "v"];
     
     public FormattedImage Process(FormattedImage image, ILogger logger, CommandCollection commands, CommandParser parser,
         CultureInfo culture)
